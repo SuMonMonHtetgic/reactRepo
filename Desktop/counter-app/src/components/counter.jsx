1a9f9d7680a6192101c9c7278bfbe6ed7b23e3e0
SuMonMonHtetgic <sumonmonhtet@gicjp.com>
2023-06-22 16:23:37 +0630

    updated at 6/22/23

--- Desktop/counter-app/src/components/counter.jsx
+++ Desktop/counter-app/src/components/counter.jsx
@@ -2,69 +2,51 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        imageUrl: "https://miro.medium.com/v2/resize:fit:1400/format:webp/0*8K2hsvp6FclU4r35.png",
-        tags: ['tag1', 'tag2', 'tag3']
+        value: this.props.counter.value       
     };
+   
     styles ={
-        fontSize: 50,
+        fontSize: 20,
         fontWeight: "bold",
         fontFamily: "cursive"
     };
 
-    // constructor(){
-    //     super();
-    //     this.handleIncrement =  this.handleIncrement.bind(this);
-    // }
-    
-    render() { 
-        // let classes = this.getBadgeClasses();
-        
-        return (
-            // <React.Fragment>
-            <div>
-                <img src={this.state.imageUrl} alt="" width={400}/> <br /><br />
-                {/* <span>{this.state.count}</span> */}
-                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                {/* <span>{2 + 3}</span> */}
-                <button 
-                onClick={this.handleIncrement} className='btn btn-primary btn-sm m-2'>Increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key={tag}>{ tag }</li> )}
-                    
-                </ul>
-                <div> {this.renderTags()}</div>
-            </div>
-          
-          
-            // </React.Fragment>
-        );
-       
-    }
-    
-    handleIncrement = () => {
-        // console.log('Increment clicked', this)
-        this.state.count++;
-        this.setState({ count: this.state.count });
-    };
 
-    renderTags(){
-        if (this.state.tags.length === 0) return <p>There are no tags</p>;
-
-        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li> )}</ul>;
-    }
-
+  
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
+        const { value } = this.props.counter;
         const text = "Zero";
-        return count === 0 ? text : count;
+        return value === 0 ? text : value;
     }
+  
+    
+    render() { 
+       
+        // console.log(this.props);
+        return (
+            
+            <div>
+               
+                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                onClick={ () => this.props.onIncrement(this.props.counter) } className='btn btn-success btn-sm m-2'>Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+               
+            </div>
+          
+          
+           
+        );
+       
+    }
+    
+   
 }
  
 export default Counter;
--- Desktop/counter-app/src/index.js
+++ Desktop/counter-app/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
+import Testing from './components/testing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
+   
   </React.StrictMode>
 );
 
